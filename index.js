3fda0a0f238d3b8926abd8844edfdec6892f5222
HyunnoH <hyunnoh01@gmail.com>
2019-10-10 12:18:15 +0900

    edit

--- index.js
+++ index.js
@@ -1,34 +1,35 @@
-const { app, BrowserWindow, ipcMain } = require("electron");
+const { app, BrowserWindow, ipcMain } = require('electron');
 
 let win;
 
-const data = require("./lib/common/data.json");
+const data = require('./lib/common/data.json');
 
-app.on("ready", () => {
+app.on('ready', () => {
   win = new BrowserWindow({
     kiosk: true,
     // autoHideMenuBar: true,
     webPreferences: {
-      nodeIntegration: true
-    }
+      nodeIntegration: true,
+    },
   });
 
+  win.webContents.openDevTools();
   win.loadURL(`file://${__dirname}/public/html/index.html`);
-  win.on("closed", () => {
+  win.on('closed', () => {
     win = null;
   });
 });
 
-app.on("windows-all-closed", () => {
-  if (process.platform !== "darwin") app.quit();
+app.on('windows-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
 });
 
-app.on("activate", () => {
+app.on('activate', () => {
   if (win === null) {
     createWindow();
   }
 });
 
-ipcMain.on("json-data-request", event => {
-  event.reply("json-data-response", data);
+ipcMain.on('json-data-request', (event) => {
+  event.reply('json-data-response', data);
 });
